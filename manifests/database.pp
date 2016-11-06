8cfc7b4da4114fc2dce9731b0dfa72a7f9243e86
brad <braddeicide@hotmail.com>
2016-11-06 18:36:15 +1100

    Remove requirement to install epel before running puppet

--- manifests/database.pp
+++ manifests/database.pp
@@ -1,23 +1,21 @@
 # Set up the pulp database
 class pulp::database {
   if $pulp::manage_db {
-    if (versioncmp($::mongodb_version, '2.6.5') >= 0) {
-      $mongodb_pidfilepath = '/var/run/mongodb/mongod.pid'
-    } else {
-      $mongodb_pidfilepath = '/var/run/mongodb/mongodb.pid'
+    exec { "Mongo_pid":
+      command => "ln /var/run/mongodb/mongod.pid /var/run/mongodb/mongodb.pid",
+      path    => "/usr/local/bin:/bin",
+      creates => "/var/run/mongodb/mongod.pid",
+      before  => Service['mongodb'],
     }
 
-    class { '::mongodb::server':
-      pidfilepath => $mongodb_pidfilepath,
-    }
-
+    class { '::mongodb::server': }
+  
     Service['mongodb'] -> Service['pulp_celerybeat']
     Service['mongodb'] -> Service['pulp_workers']
     Service['mongodb'] -> Service['pulp_resource_manager']
     Service['mongodb'] -> Service['pulp_streamer']
     Service['mongodb'] -> Exec['migrate_pulp_db']
   }
-
   exec { 'migrate_pulp_db':
     command   => 'pulp-manage-db && touch /var/lib/pulp/init.flag',
     path      => '/bin:/usr/bin',
